805f29749e1e9e0d0f17d1bb2e9348d00a237ece
InHoYi <inhomike17@gmail.com>
2025-04-01 20:41:00 +0900

    버전1

--- rag1.py
+++ rag1.py
@@ -1,18 +1,21 @@
 from langchain_community.document_loaders import WebBaseLoader
-from langchain_community.document_loaders import PyPDFLoader
-from langchain.chains.summarize import load_summarize_chain
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
 model = OllamaLLM(model="exaone3.5:latest")
 
-loader = WebBaseLoader('https://news.naver.com')
+
+loader = WebBaseLoader('원하는 URL')
 docs = loader.load()
-text_data = [doc.page_content for doc in docs]
 
+template = ChatPromptTemplate.from_template("""
+    질문에 대해서 context 부분을 읽고 답변을 한국어로 작성해줘.
+    context : {context}
+    질문 : {query}
+    답변 : 
+""")
 
-chain = load_summarize_chain(model, chain_type = "stuff") 
-# stuff 옵션 -> 다 집어넣어주세요.
-# map_reduce 옵션 -> 길이가 긴 경우
-result = chain.invoke(text_data)
+chain = template | model | StrOutputParser()
+result = chain.invoke({'context' : docs, 'query' : "요약해줘."})
 print(result)
