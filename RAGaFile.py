c1565a755e8f9a851ad612ba88225a8fa979cb23
InHoYi <inhomike17@gmail.com>
2025-04-02 10:13:54 +0900

    template 수정

--- RAGaFile.py
+++ RAGaFile.py
@@ -16,12 +16,12 @@ def loadDocument(dir):
 document = loadDocument(f'article/article{datetime.today().strftime("%Y%m%d")}.txt')
 
 template = ChatPromptTemplate.from_template("""
-    질문에 대해서 context 부분을 읽고 답변을 한국어로 작성해줘.
+    질문에 대해서 context 부분을 읽고 query에 대한 답변을 한국어로 작성해줘.
     context : {context}
     질문 : {query}
     답변 : 
 """)
 
 chain = template | model | StrOutputParser()
-result = chain.invoke({'context' : document, 'query' : "요약해줘."})
+result = chain.invoke({'context' : document, 'query' : "주요 뉴스를 요약해줘."})
 print(result)
